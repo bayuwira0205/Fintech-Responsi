8b08a25b93494165835293deee7a25d129b0b8d3
bayu wira <75984475+bayuwira0205@users.noreply.github.com>
2021-07-07 01:01:57 +0700

    Add files via upload
    desain ui 1

---
+++ responsifintech/app/src/main/java/com/example/responsifintech/recovery.kt
@@ -0,0 +1,11 @@
+package com.example.responsifintech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recovery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recovery)
+    }
+}
